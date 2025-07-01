9f07da79a3f91cd54fcfb96792be65827c9cbf1b
Matt Graham <matt@hooligeek.com>
2025-07-01 09:59:34 -0500

    Added log handling

--- upgrade_bedrock.sh
+++ upgrade_bedrock.sh
@@ -49,6 +49,6 @@ rm -rf "$TEMP_DIR"
 # Step 7: Restart the server in a screen session
 echo "Starting Minecraft Bedrock server in screen session '$SCREEN_NAME'..."
 cd "$SERVER_DIR"
-screen -dmS "$SCREEN_NAME" ./bedrock_server
+screen -dmS "$SCREEN_NAME" ./bedrock_server > bedrock_server.log 2>&1
 
 echo "Upgrade complete!"
